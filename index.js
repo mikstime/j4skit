5350d94a90cc730f2e23f10d2074b9a528d2d56d
Michael Balitsky <35119841+mikstime@users.noreply.github.com>
2020-05-30 14:15:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Page = import('./page.js');
 const Navigator = import('./page.js');
-console.log(Page)
-console.log(Navigator.version);
-console.log(Page.version);
+
 module.exports = {
   Navigator,
   Page,
